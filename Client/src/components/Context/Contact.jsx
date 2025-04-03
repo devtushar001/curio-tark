b750910081e912a07b91f9d289845a443dfbdde1
devtushar001 <tc341269@gmail.com>
2025-04-03 00:00:00 -0700

    backend url changed

--- Client/src/components/Context/Contact.jsx
+++ Client/src/components/Context/Contact.jsx
@@ -8,7 +8,7 @@ const DochakiContextProvider = (props) => {
     const [cartItem, setCartItem] = useState({});
     const [token, setToken] = useState("");
     const [bikeAccessories, setBikeAccessories] = useState([]);
-    const url = 'http://localhost:10019';
+    const url = 'https://quro-tark-backend.onrender.com';
 
     const fetchBikeAccessoriesList = async () => {
         try {
